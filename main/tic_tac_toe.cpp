26825f2865ea75a948ae86f0aefd2dfac04a03ce
Marcus Drohse <Marcusdrohse@gmail.com>
2026-03-27 21:16:34 +0100

    Added ability to switch turn - Input handling

--- main/tic_tac_toe.cpp
+++ main/tic_tac_toe.cpp
@@ -25,7 +25,7 @@ void print_board(string board[3][3]) {
         cout << endl;
     }
     cout << "---+---+---+---+" << endl;
-    cout << "   | A | B | C |" << endl << endl;
+    cout << "   | 1 | 2 | 3 |" << endl << endl;
 
 }
 
@@ -37,8 +37,67 @@ int main() {
     };
 
     bool player_won = false;
+    string player_turn = "X";
+    int turn_count = 0;
 
-    print_board(board);
+    while (player_won == false) {
+        bool player_input = false;
+
+        // checks if the player input is valid - if not try again
+        while (player_input == false) {
+            int row;
+            int column;
+
+            cout << turn_count;
+
+            if (turn_count == 4) {  //
+                player_won = true;  // simple game stopper after 4 moves
+            }                       //
+
+            cout << endl << "Please enter row: ";
+            cin >> row;
+            // check if input failed
+            while (cin.fail()) {
+                cout << "Invalid row! Please use 1-3" << endl;
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cin >> row;
+            }
+
+            cout << endl << "Please enter column: ";
+            cin >> column;
+            // check if input failed and try again
+            while (cin.fail()) {
+                cout << "Invalid column! Please use 1-3" << endl;
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cin >> column;
+            }
+
+            // checks if the row/column is in the correct number range
+            if (row >= 1 && row <= 3) {
+                if (column >= 1 && column <= 3) {
+                    board[row-1][column-1] = player_turn;
+                    if (player_turn == "X") {
+                        player_turn = "O";
+                    } else {
+                        player_turn = "X";
+                    }
+                    turn_count += 1;
+                    print_board(board);
+                    player_input = true;
+                } else {
+                    column = 0;
+                    cout << "Invalid column! Please use 1-3" << endl;
+                }
+            } else {
+                row = 0;
+                cout << "Invalid row! Please use 1-3" << endl;
+            }
+        }
+    }
+
+    //print_board(board);
 
     return 0;
 }
